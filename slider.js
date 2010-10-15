6ded376a5ae9ff83b476bbee280d570ce5641307
Frank Yan <studio17@gmail.com>
2010-10-15 11:01:51 -0500

    implemented properties and attributes correctly

--- slider.js
+++ slider.js
@@ -25,7 +25,7 @@ THE SOFTWARE.
 // test for native support
 var test = document.createElement('input');
 test.type = 'range';
-if (test.value == '50')
+if (test.value == 50)
   return;
 // test for required CSS property support
 if (!('boxShadow' in test.style) || !('MozAppearance' in test.style))
@@ -49,84 +49,81 @@ document.addEventListener('DOMNodeInserted', function(e) {
 function create(slider) {
 
   function onDragStart(e) {
-    var mid = (this.min - -this.max) / 2;
-    var multiplier = origWidth / (this.max - this.min);
-    var dev = Math.abs(this.value - mid) * multiplier;
     var x = e.clientX - this.offsetLeft;
     // distance between click and center of nub
     var diff = x - origWidth / 2 - dev;
     // check if click was within control bounds
-    // TODO: detect clicking on right end when nub is in left half
-    if (!(this.value < mid ? x > 2 * dev - 5 : x < origWidth + 10))
+    if (!(value < mid ? x > 2 * dev - 5 : x < origWidth + 10))
       return;
     // if click was not on nub, move nub to click location
-    // TODO: center nub at click location
-    if (diff < -3 || diff > 7) {
+    if (diff < -3 || diff > 7)
       this.value -= -diff / multiplier;
-      draw.call(this);
-    }
-    dragging = 1;
-    tempValue = this.value;
+    tempValue = value;
     prevX = e.clientX;
+    addEventListener('mousemove', onDrag, false);
+    addEventListener('mouseup', onDragEnd, false);
   }
 
   function onDrag(e) {
-    if (!dragging)
-      return;
-    tempValue -= (prevX - e.clientX) * (this.max - this.min) / origWidth;
+    tempValue += (e.clientX - prevX) / multiplier;
     prevX = e.clientX;
-    this.value = tempValue;
-    draw.call(this);
+    slider.value = tempValue;
   }
 
-  function onDragEnd() {
-    dragging = 0;
+  function onDragEnd(e) {
+    removeEventListener('mousemove', onDrag, false);
+    removeEventListener('mouseup', onDragEnd, false);
   }
 
-  function setWidth(width) {
+  // determines whether value is valid number in attribute form
+  function isAttrNum(value) {
+    return !isNaN(value) && +value == parseFloat(value, 10);
+  }
+
+  function resetWidth(width) {
     if (this != slider)
-      throw new TypeError('Illegal Invocation');
-    origWidth = +('' + width) || origWidth ||
-                parseInt(getComputedStyle(this, 0).width);
-    reset.call(this);
+      throw new TypeError('Illegal invocation');
+    if (isAttrNum(width)) {
+      origWidth = +width;
+      reset.call(this);
+    }
   }
 
-  // validates min, max, and step attributes and redraws
+  // validates min, max, and step attributes/properties and redraws
   function reset() {
-    var min = '' + this.getAttribute('min');
-    var max = '' + this.getAttribute('max');
-    var step = '' + this.getAttribute('step');
-    // if invalid, reset min to 0, max to 100, and/or step to 1
-    this.min = isNaN(min) ? 0 : +min;
-    this.max = isNaN(max) || max < min ? 100 : +max;
-    this.step = isNaN(step) ? 1 : +step;
+    min = isAttrNum(this.min) ? +this.min : 0;
+    max = isAttrNum(this.max) ? +this.max : 100;
+    step = isAttrNum(this.step) ? +this.step : 1;
+    // cache common computations
+    mid = (min + max) / 2;
+    range = max - min;
+    // draw only nub if min equals max (WebKit does otherwise)
+    multiplier = range ? origWidth / range : 1;
     draw.call(this, 1);
+  }
+
+  // recalculates value property
+  function calc() {
+    if (!isAttrNum(value))
+      value = mid;
+    // snap to step intervals (WebKit sometimes does not - bug?)
+    value = Math.round((value - min) / step) * step + min;
+    // clamp to [min, max]
+    if (value < min)
+      value = min;
+    else if (value > max)
+      value = min + ~~(range / step) * step;
+    dev = Math.abs(value - mid) * multiplier;
   }
 
   // renders slider using CSS width, margin, and box-shadow
   function draw(force) {
-    var value = '' + this.getAttribute('value');
-    // if invalid, reset value to mean of min and max
-    if (isNaN(value))
-      value = (this.min - -this.max) / 2;
-    // snap to step intervals
-    value = Math.round((value - this.min) / this.step);
-    value = value * this.step - -this.min;
-    // clamp to [min, max]
-    if (value < this.min)
-      value = +this.min;
-    else if (value > this.max)
-      value = +this.min + ~~((this.max - this.min) / this.step) * this.step;
-    this.setAttribute('value', value);
+    calc();
     // prevent unnecessary redrawing
     if (!force && value == prevValue)
       return;
     prevValue = value;
     // render it!
-    var mid = (this.min - -this.max) / 2;
-    var range = this.max - this.min;
-    var multiplier = origWidth / range;
-    var dev = Math.abs(value - mid) * multiplier;
     this.style.width = origWidth + 2 * dev + 'px';
     this.style.marginLeft = (value < mid ? -dev * 2 : 0) + 5 + 'px';
     this.style.marginRight = (value > mid ? -dev * 2 : 0) + 5 + 'px';
@@ -134,54 +131,65 @@ function create(slider) {
     // experimental thin style
     if (~this.className.split(' ').indexOf('x-thin'))
       style = 'px 2px 0 -6px #555';
-    var end = (this.max - value) * multiplier;
-    for (var i = (this.min - value) * multiplier; i < end; i += 3)
+    var end = (max - value) * multiplier;
+    for (var i = (min - value) * multiplier; i < end; i += 3)
       shadow.push(i + style);
     shadow.push(end + style);
     this.style.boxShadow = shadow.join();
   }
 
-  // variables kept private within the closure
-  var origWidth, prevValue, dragging, tempValue, prevX;
+  var origWidth, value, min, max, step, mid, range, multiplier, dev;
+  var isValueModified, prevValue, tempValue, prevX;
 
-  // create slider affordance
-  slider.style.MozAppearance = 'radio';
-  // TODO: integrate -moz-grab(bing) cursor?
-  slider.style.cursor = 'default';
-
-  // TODO: find better way to set width on-the-fly
-  slider.setWidth = function() {
-    setWidth.apply(this, arguments);
-  };
+  // since changes before this are unknown, assume this is initial value
+  if (slider.value !== '')
+    value = slider.value;
+  // implement value property properly
+  slider.__defineGetter__('value', function() {
+    calc();
+    return '' + value;
+  });
+  slider.__defineSetter__('value', function(val) {
+    value = '' + val;
+    isValueModified = true;
+    draw.call(this);
+  });
 
   // sync properties with attributes
-  ['value', 'min', 'max', 'step'].forEach(function(attr) {
-    slider.__defineGetter__(attr, function() {
-      return this.getAttribute(attr);
+  ['min', 'max', 'step'].forEach(function(prop) {
+    slider.__defineGetter__(prop, function() {
+      return this.hasAttribute(prop) ? this.getAttribute(prop) : '';
     });
-    slider.__defineSetter__(attr, function(val) {
-      this.setAttribute(attr, val);
+    slider.__defineSetter__(prop, function(val) {
+      val === null ? this.removeAttribute(prop) : this.setAttribute(prop, val);
     });
   });
 
-  // TODO: defer attribute initialization until manually set
-  slider.setWidth();
+  // create slider affordance
+  slider.style.MozAppearance = 'radio';
+  slider.style.cursor = 'default';
 
-  // TODO: verify that this is not triggered excessively
+  // expose public method to reset width on-the-fly safely
+  slider.resetWidth = function() {
+    resetWidth.apply(this, arguments);
+  };
+
+  // initialize slider
+  slider.resetWidth(parseFloat(getComputedStyle(slider, 0).width));
+
   slider.addEventListener('DOMAttrModified', function(e) {
-    if (~['min', 'max', 'step'].indexOf(e.attrName))
-      reset.call(this);
-    else if (e.attrName == 'value')
+    // note that value attribute only sets initial value
+    if (e.attrName == 'value' && !isValueModified) {
+      value = e.newValue;
       draw.call(this);
+    }
+    else if (~['min', 'max', 'step'].indexOf(e.attrName))
+      reset.call(this);
   }, false);
 
   slider.addEventListener('mousedown', onDragStart, false);
-  slider.addEventListener('mousemove', onDrag, false);
-  slider.addEventListener('mouseout', onDragEnd, false);
-  slider.addEventListener('mouseup', onDragEnd, false);
 
   // stop the focus ring from distorting the box shadow
-  // TODO: find better way to do this for accessibility reasons
   slider.addEventListener('focus', function() {
     this.blur();
   }, false);
